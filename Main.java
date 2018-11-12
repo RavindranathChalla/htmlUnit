4b27ac9f24bf23b156785a2cade1c17725d75370
RavindranathChalla <33298271+RavindranathChalla@users.noreply.github.com>
2018-11-11 23:04:40 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,12 +4,20 @@ import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.SilentCssErrorHandler;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import com.gargoylesoftware.htmlunit.javascript.JavaScriptEngine;
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String... args) throws IOException {
         WebClient webClient = new WebClient(BrowserVersion.CHROME);
+       // JavaScriptEngine engine = webClient.getJavaScriptEngine();
+        //engine.holdPosponedActions();
         webClient.setCssErrorHandler(new SilentCssErrorHandler());
         webClient.setAjaxController(new NicelyResynchronizingAjaxController());
         webClient.getOptions().setUseInsecureSSL(true);
@@ -21,9 +29,29 @@ public class Main {
         webClient.getOptions().setTimeout(10000);
         webClient.getOptions().setThrowExceptionOnScriptError(false);
         webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
-        HtmlPage page = webClient.getPage("http://www.google.com");
+
+        HtmlPage page = webClient.getPage("https://www.google.com/");
         //System.out.println(page.asXml());
+       // final HtmlForm form = page.getFormByName("frm");
+        final HtmlAnchor button = (HtmlAnchor) page.getElementById("gb_70");
+        //final HtmlSubmitInput button = form.getInputByName("Commit");
+        //final HtmlTextInput textField = form.getInputByName("sqlstring");
+
         System.out.println("Title-->"+page.getTitleText());
+        
+		System.out.println("button--"+button);
+		
+        // Change the value of the text field
+        //textField.type("root");
+        
+       // System.out.println("textField--"+textField);
+
+        // Now submit the form by clicking the button and get back the second page.
+        final HtmlPage page2 = button.click();
+        final HtmlTextInput textField = page2.getElementByName("identifier");
+        textField.type("ravinathchalla@gmail.com");
+        System.out.println("textField--"+textField);
+       
         webClient.closeAllWindows();
     }
 }
