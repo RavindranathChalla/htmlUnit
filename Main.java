1566ac9a1157c5338affe1725c30930afb31201c
RavindranathChalla <33298271+RavindranathChalla@users.noreply.github.com>
2018-11-15 10:48:23 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.SilentCssErrorHandler;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
@@ -12,6 +13,7 @@ import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 import com.gargoylesoftware.htmlunit.javascript.JavaScriptEngine;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String... args) throws IOException {
@@ -29,7 +31,26 @@ public class Main {
         webClient.getOptions().setTimeout(10000);
         webClient.getOptions().setThrowExceptionOnScriptError(false);
         webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+        HtmlPage page = webClient.getPage("http://htmlunit.sourceforge.net");
+        Iterable<DomElement> childEle;
+        List<DomElement> divs = page.getElementsByTagName("div");
+         for(DomElement element : divs){
+             if(element.getAttribute("class").equals("well sidebar-nav")){
+             childEle=element.getChildElements();
+             for(DomElement ce: childEle){
+                System.out.println(ce);
+                  Iterable<DomElement> de=ce.getChildElements();
+                  for(DomElement d:de){
+                             if(d.getByXPath("//a").contains("Project Page")){
+                  System.out.println(d.toString());
+                  System.out.println( d.getByXPath("//a"));
+                             }
+                      }
+             }
+             }
+         }
 
+        //HtmlPage page = webClient.getPage("https://wuapp-prodrs.westernunion.net/RMP/jsp/update1.jsp");
         HtmlPage page = webClient.getPage("https://www.google.com/");
         //System.out.println(page.asXml());
        // final HtmlForm form = page.getFormByName("frm");
@@ -39,7 +60,7 @@ public class Main {
 
         System.out.println("Title-->"+page.getTitleText());
         
-		System.out.println("button--"+button);
+		System.out.println("button--"+button.asText());
 		
         // Change the value of the text field
         //textField.type("root");
@@ -50,7 +71,7 @@ public class Main {
         final HtmlPage page2 = button.click();
         final HtmlTextInput textField = page2.getElementByName("identifier");
         textField.type("ravinathchalla@gmail.com");
-        System.out.println("textField--"+textField);
+        System.out.println("textField--"+textField.getValueAttribute());
        
         webClient.closeAllWindows();
     }
